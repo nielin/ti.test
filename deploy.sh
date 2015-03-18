8cd4ab6eca6a6f570f995f6ebddf4ff7690f78ee
Appcelerator Build <build@users.noreply.github.com>
2015-03-17 23:49:19 -0700

    Added Android builds

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ echo "{ \"type\": \"timodule\", \"access\": \"public\", \"metadata\": { \"platfo
 echo "{ \"name\": \"ti.styledlabel\", \"version\": \"1.6.1\", \"description\": \"ti.styledlabel module\" }" > ios/package.json
 cd ios
 appc-reg --token "$TOKEN"
+cd ../android/dist
+echo "{ \"type\": \"timodule\", \"access\": \"public\", \"metadata\": { \"platform\": \"android\" } }" > android/dist/appc.json
+echo "{ \"name\": \"ti.styledlabel\", \"version\": \"2.0.2\", \"description\": \"ti.styledlabel module\" }" > android/dist/package.json
+appc-reg --token "$TOKEN"
