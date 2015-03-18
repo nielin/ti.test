6fde18014a548a31cd3ee1773d2820a557e219f2
Appcelerator Build <build@users.noreply.github.com>
2015-03-17 23:41:47 -0700

    Appeasing Jeff

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export APPC_ENV=preproduction
-export TOKEN=`echo token.txt`
+export TOKEN="`cat token.txt`"
 sudo npm install appc-reg -g
 echo "{ \"type\": \"timodule\", \"metadata\": { \"platform\": \"ios\" } }" > ios/appc.json
 echo "{ \"name\": \"ti.styledlabel\", \"version\": \"1.6.1\", \"description\": \"ti.styledlabel module\" }" > ios/package.json
