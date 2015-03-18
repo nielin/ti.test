1d8963a1c02351ed48cd962683225fbffd1ddc99
Appcelerator Build <build@users.noreply.github.com>
2015-03-17 23:53:07 -0700

    Use proper directories

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 export APPC_ENV=preproduction
 export TOKEN="`cat token.txt`"
 sudo npm install appc-reg -g
-echo "{ \"type\": \"timodule\", \"access\": \"public\", \"metadata\": { \"platform\": \"ios\" } }" > ios/appc.json
-echo "{ \"name\": \"ti.styledlabel\", \"version\": \"1.6.1\", \"description\": \"ti.styledlabel module\" }" > ios/package.json
-cd ios
+echo "{ \"type\": \"timodule\", \"access\": \"public\", \"metadata\": { \"platform\": \"ios\" } }" > $MODULE_ROOT/ios/appc.json
+echo "{ \"name\": \"ti.styledlabel\", \"version\": \"1.6.1\", \"description\": \"ti.styledlabel module\" }" > $MODULE_ROOT/ios/package.json
+cd $MODULE_ROOT/ios
 appc-reg --token "$TOKEN"
-cd ../android/dist
-echo "{ \"type\": \"timodule\", \"access\": \"public\", \"metadata\": { \"platform\": \"android\" } }" > android/dist/appc.json
-echo "{ \"name\": \"ti.styledlabel\", \"version\": \"2.0.2\", \"description\": \"ti.styledlabel module\" }" > android/dist/package.json
+echo "{ \"type\": \"timodule\", \"access\": \"public\", \"metadata\": { \"platform\": \"android\" } }" > $MODULE_ROOT/android/dist/appc.json
+echo "{ \"name\": \"ti.styledlabel\", \"version\": \"2.0.2\", \"description\": \"ti.styledlabel module\" }" > $MODULE_ROOT/android/dist/package.json
+cd $MODULE_ROOT/android/dist
 appc-reg --token "$TOKEN"
