95746cdce02426630ed73d48ce3b8be6a1919260
Appcelerator Build <build@users.noreply.github.com>
2015-03-17 23:47:27 -0700

    Adding public access

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 export APPC_ENV=preproduction
 export TOKEN="`cat token.txt`"
 sudo npm install appc-reg -g
-echo "{ \"type\": \"timodule\", \"metadata\": { \"platform\": \"ios\" } }" > ios/appc.json
+echo "{ \"type\": \"timodule\", \"access\": \"public\", \"metadata\": { \"platform\": \"ios\" } }" > ios/appc.json
 echo "{ \"name\": \"ti.styledlabel\", \"version\": \"1.6.1\", \"description\": \"ti.styledlabel module\" }" > ios/package.json
 cd ios
 appc-reg --token "$TOKEN"
